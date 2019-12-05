32cbdfb6090cd1fd021dd99cdefa3e0f5191fe2a
Pikakashi <47730246+Pikakashi@users.noreply.github.com>
2019-12-05 09:58:00 -0800

    added toString()

--- Amenity.java
+++ Amenity.java
@@ -8,4 +8,13 @@ public abstract class Amenity extends Room
 	 * @return The description of the Room and its Amenities.
 	 */
 	public abstract String getDescription();
+	
+	/**
+	 * Returns a String representation of the Room and its Amenities
+	 * @return A String representation of the Room and its Amenities
+	 */
+	public String toString()
+	{
+		return getDescription();
+	}
 }
