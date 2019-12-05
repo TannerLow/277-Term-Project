58f6666fa7bc75ab7cc89cf135f72ff22ac3e0a8
Pikakashi <47730246+Pikakashi@users.noreply.github.com>
2019-12-05 10:52:30 -0800

    method comments to RoomVisitor.java

--- RoomVisitor.java
+++ RoomVisitor.java
@@ -1,7 +1,18 @@
 
 public interface RoomVisitor {
-	//implement different types of rooms
+	/**
+	 * Visit a regular room
+	 * @param reg room to visit
+	 */
 	public void visit(RegularRoom reg);
+	/**
+	 * Visit a deluxe room
+	 * @param del room to visit
+	 */
 	public void visit(DeluxeRoom del);
+	/**
+	 * Visit a suite room
+	 * @param suite room to visit
+	 */
 	public void visit(SuiteRoom suite);
 }
