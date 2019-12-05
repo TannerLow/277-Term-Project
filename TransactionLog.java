9b0735b1728064fc0dc3aa44530c8811028e9cc6
Pikakashi <47730246+Pikakashi@users.noreply.github.com>
2019-12-05 10:55:00 -0800

    method comments to transactionLog.java

--- TransactionLog.java
+++ TransactionLog.java
@@ -3,14 +3,25 @@ import java.util.ArrayList;
 public class TransactionLog {
 	private ArrayList<String> transactions;
 	
+	/**
+	 * Creates a TransactionLog
+	 */
 	public TransactionLog() {
 		transactions = new ArrayList<>();
 	}
 	
+	/**
+	 * Add a transaction to the log
+	 * @param s The transcation to add
+	 */
 	public void addTransaction(String s) {
 		transactions.add(s);
 	}
 	
+	/**
+	 * Returns a String representation of the log
+	 * @return A String representation of the log
+	 */
 	public String toString() {
 		String s = "";
 		for(String t : transactions) {
