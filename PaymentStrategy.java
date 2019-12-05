d3e2b42ff82ab6357bdaabc678e9743a5b4cf1fa
Pikakashi <47730246+Pikakashi@users.noreply.github.com>
2019-12-05 10:12:58 -0800

    javadoc comments to PaymentStrategy.java

--- PaymentStrategy.java
+++ PaymentStrategy.java
@@ -1,4 +1,11 @@
-
+/**
+ * Objects inheriting PaymentStrategy are able to pay(value) for a value
+ */
 public interface PaymentStrategy {
+	/**
+	 * Pay for an amount
+	 * @param amount Amount to pay for
+	 * @return String
+	 */
 	 public String pay(int amount);
 }
